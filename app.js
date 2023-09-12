bb55709275cb33449bb6d807bc8ad672921477d0
fenrir <darsha019@gmail.com>
2023-09-12 13:33:52 +0530

    added compressor

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const compression = require("compression")
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 require("dotenv").config()
 
@@ -15,6 +15,8 @@ mongoose.set("strictQuery", false)
 var app = express();
 
 mongoose.connect(process.env.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,10 +26,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
